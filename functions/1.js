472aa5d897539474e1ca8e0d54afa70d859f3955
Bruno Brito <bruno@email.com>
2022-04-27 22:21:55 -0300

    122111

---
+++ functions/1.js
@@ -0,0 +1,14 @@
+       function addCPF(){     
+            var newData = app.data.map((i) => {
+                return {
+                    "email": i.email,
+                    "status": i.status,
+                    "cpf": "370.176.868-45",
+                       "testinho" : true
+                }
+            }) 
+
+            return newData
+        } 
+
+        modules.exports = addCPF() 
